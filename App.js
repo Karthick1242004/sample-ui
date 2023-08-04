cc3b9c6661e265b4c169b4ea13fbac7efce9eb08
karthick <93571352+Karthick1242004@users.noreply.github.com>
2023-08-04 13:55:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,178 @@
+
+import './App.css';
+import logo1 from './My project (1).png';
+import logo2 from './pexels-fauxels-3182834.jpg';
+import logo3 from './photo-output-modified.png';
+import logo4 from './pie.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <ul>Solutions</ul>
+        <ul>Solutions</ul>
+        <ul>Solutions</ul>
+        <ul><button className="btn1">Sign Up</button></ul>
+      </nav>
+      <div className="logo">
+        <h1>UI/UX Design.</h1>
+      </div>
+      <div >
+        <h1 className="three">3</h1>
+        <h1 className="four"> Years Experienced<br></br>In Automation.</h1>
+      </div>
+      <div className="five">
+        <h1>Best Experience<br></br>Management<br></br>Automation.</h1>
+      </div>
+      <div className="six">
+        <p>Automation is benificial in hazordous environments<br></br>where human presence may poses a risk.</p>
+      </div>
+      <div>
+        <button  className="button" type="submit">Purchase Now</button>
+      </div>
+      <div className='connt'>
+      <img src={logo4} className="logo4" alt="logo" />
+      </div>
+      <div className="cont">
+      <img src={logo3} className="logo3" alt="logo" />
+        <h3>Karthickrajan S</h3>
+        <p>Project Organiser</p>
+      </div>
+      <img src={logo1} className="logo1" alt="logo" />
+      <div>
+        <p className="th">Over 32+k software businesses growing with D-Agency.</p>
+      </div>
+      <div>
+        <h1 className="serv">Our <span>INCLUDED</span><br></br>Services.</h1>
+      </div>
+      <div className="mncont">
+        <div className="mncont1">
+        <div className='icon'><FontAwesomeIcon icon={faInstagram} /></div>
+          <h1>UI/UX Design</h1>
+          <p>We provide quality and unique services</p><br></br>
+          <a href="#">Learn More--></a>
+        </div>
+        <div className="mncont1">
+        <div className='icon'><FontAwesomeIcon icon={faInstagram} /></div>
+          <h1>UI/UX Design</h1>
+          <p>We provide quality and unique services</p><br></br>
+          <a href="#">Learn More--></a>
+        </div>
+        <div className="mncont1">
+        <div className='icon'><FontAwesomeIcon icon={faInstagram} /></div>
+          <h1>UI/UX Design</h1>
+          <p>We provide quality and unique services</p><br></br>
+          <a href="#">Learn More--></a>
+        </div>
+        <div className="mncont1">
+        <div className='icon'><FontAwesomeIcon icon={faInstagram} /></div>
+          <h1>UI/UX Design</h1>
+          <p>We provide quality and unique services</p><br></br>
+          <a href="#">Learn More--></a>
+        </div>
+      </div>
+      <div className='flx'>
+      <img src={logo2} className="logo2" alt="logo" />
+      <div className='unq'>
+        <div className='our'>
+        <h1>Our Unique Approach</h1>
+        <p>The most common theoretical approach to agency is one that views agents as individuals<br></br>and politics as a realm constituted by individual agents.</p>
+        </div>
+        <div className='off1'>
+          <h1>Offers the global standard service to <br></br>buisness </h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus omnis fugit<br></br> repellat similique eos, doloribus provident exercitationem amet ducimus, quo asperiores<br></br>aspernatur consectetur incidunt quidem natus adipisci corrupti enim?</p>
+        </div>
+        <div className='off2'>
+          <h1>Offers the global standard service to <br></br>buisness </h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus omnis fugit<br></br> repellat similique eos, doloribus provident exercitationem amet ducimus, quo asperiores<br></br>aspernatur consectetur incidunt quidem natus adipisci corrupti enim?</p>
+        </div>
+      </div>
+      </div>
+      <div className='icn1'>
+      <a href='#' className='i1'><FontAwesomeIcon icon={faInstagram} /></a>
+      <a href='#' className='i2'><FontAwesomeIcon icon={faInstagram} /></a>
+      </div>
+      <div className='prc'>
+        <h1>Pricing Plan Made For Your<br></br>Comfort Purchase</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus omnis fugit<br></br> repellat similique eos, doloribus provident exercitationem amet ducimus,<br></br>quo asperiores<br></br></p>
+      </div>
+      <div className='cont4'>
+        <div className='cont41'>
+          <h3>For Starters</h3>
+          <h1>$2999</h1>
+          <a href="#"><FontAwesomeIcon icon={faCheck}/></a><p>Feedback Categorization</p>
+          <a href="#"><FontAwesomeIcon icon={faCheck}/></a><p>Feedback Categorization</p>
+          <a href="#"><FontAwesomeIcon icon={faCheck}/></a><p>Feedback Categorization</p>
+          <a href="#"><FontAwesomeIcon icon={faCheck}/></a><p>Feedback Categorization</p>
+          <button  className="button2" type="submit">Purchase Now</button>
+        </div>
+        <div className='cont41'>
+          <h3>For Starters</h3>
+          <h1>$2999</h1>
+          <a href="#"><FontAwesomeIcon icon={faCheck}/></a><p>Feedback Categorization</p>
+          <a href="#"><FontAwesomeIcon icon={faCheck}/></a><p>Feedback Categorization</p>
+          <a href="#"><FontAwesomeIcon icon={faCheck}/></a><p>Feedback Categorization</p>
+          <a href="#"><FontAwesomeIcon icon={faCheck}/></a><p>Feedback Categorization</p>
+          <button  className="button2" type="submit">Purchase Now</button>
+        </div>
+        <div className='cont41'>
+          <h3>For Starters</h3>
+          <h1>$2999</h1>
+          <a href="#"><FontAwesomeIcon icon={faCheck}/></a><p>Feedback Categorization</p>
+          <a href="#"><FontAwesomeIcon icon={faCheck}/></a><p>Feedback Categorization</p>
+          <a href="#"><FontAwesomeIcon icon={faCheck}/></a><p>Feedback Categorization</p>
+          <a href="#"><FontAwesomeIcon icon={faCheck}/></a><p>Feedback Categorization</p>
+          <button  className="button2" type="submit">Purchase Now</button>
+        </div>
+      </div>
+      <div className='feedback'>
+        <div className='prof'>
+          <h1>UI/UX Design</h1><br></br>
+          <a href="#" className='ii1'><FontAwesomeIcon icon={faEnvelope} /></a>
+          <h3>uiuxinfo@gmail.com</h3><br></br>
+          <a href="#" className='ii2'><FontAwesomeIcon icon={faPhoneVolume} /></a>
+          <p>+990 1234567</p>
+        </div>
+      </div>
+      <div className='comp'>
+        <h1>Company</h1>
+        <p>About Us</p>
+        <p>Features</p>
+        <p>Watch Videos</p>
+        <p>Contact Us</p>
+      </div>
+      <div className='pol'>
+        <h1>Our Policies</h1>
+        <p>Provacy Policy</p>
+        <p>Terms Of users</p>
+        <p>Cookies Polices</p>
+        <p>GDPR Policies</p>
+      </div>
+      <div className='soc'>
+        <h1>Social Media</h1>
+        <a href="#" className='ii5'><FontAwesomeIcon icon={faInstagram} /></a><p>Instagram</p>
+        <a href="#" className='ii5'><FontAwesomeIcon icon={faLinkedin} /></a><p>LinkedIn</p>
+        <a href="#" className='ii5'><FontAwesomeIcon icon={faGithub} /></a><p>Git Hub</p>
+        <a href="#" className='ii5'><FontAwesomeIcon icon={faWhatsapp} /></a><p>WhatsApp</p>
+      </div>
+      .<div className='add'>
+        <p>4517 Washington Ave,<br></br>Manchester,<br></br>Kentucky 39495.
+        </p>
+      </div>
+      <div className='sub'>
+        <input name="name" className='inp' placeholder='Enter your name'></input>
+        <button type='submit' className='bbt'>Join</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
